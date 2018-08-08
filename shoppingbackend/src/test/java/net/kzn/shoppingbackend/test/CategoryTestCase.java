3084df36efc1fae29be0089191c3424cb5b465e7
Chalapathi N <chala.arya@gmail.com>
2018-08-08 17:45:53 +0530

    commited successfully

---
+++ shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/CategoryTestCase.java
@@ -0,0 +1,11 @@
+package net.kzn.shoppingbackend.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CategoryTestCase {
+
+	private static AnnotationConfigApplicationContext context;
+	
+	private static CategoryDAO categoryDAO;
+	
+}
